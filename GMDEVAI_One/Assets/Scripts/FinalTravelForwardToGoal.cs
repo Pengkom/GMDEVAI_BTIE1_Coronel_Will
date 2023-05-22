75174d299854187a7683a02ccf7084b1cc974794
Will Coronel <will.coronel@benilde.edu.ph>
2023-05-22 19:13:12 +0800

    Another one

--- GMDEVAI_One/Assets/Scripts/FinalTravelForwardToGoal.cs
+++ GMDEVAI_One/Assets/Scripts/FinalTravelForwardToGoal.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FinalTravelForwardToGoal : MonoBehaviour
 {
     public Transform goal;
-    private float speed = 3;
+    private float speed = 4;
     private float rotSpeed = 3;
     
     void LateUpdate() //EVERY FRAME
@@ -13,17 +13,22 @@ public class FinalTravelForwardToGoal : MonoBehaviour
         //LookAt but disregard goal's posY by looking at self posY
         Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
         
-        //Find direction by subtracting pointB to pointA
+        //Find direction by subtracting pointB to pointA [Vector3]
         Vector3 direction = lookAtGoal - transform.position;
 
         //Slowly rotate toward goal(direction)
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        
+        //FOR LERP---------------------------------------------------------------------------------------------------------------------------------
+        //Calculate distance [Distance/Magnitude]
+        float distance = Vector3.Distance(lookAtGoal, this.transform.position);
 
         //Stop movement when too close
-        if (Vector3.Distance(lookAtGoal, transform.position) > 1)
+        if (distance > 2)   
         {
             //Movement Maths
-            Vector3.Lerp(this.transform.position, goal.position, speed * Time.deltaTime);
+            this.transform.position = Vector3.Lerp(this.transform.position, lookAtGoal, (Time.deltaTime * speed) / distance);
+            
         }
         
     }
