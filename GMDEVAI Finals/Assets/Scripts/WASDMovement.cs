c3149c9436a85a6b887a14cc75305c445e55b947
dnellecruz <danielle.cruz@benilde.edu.ph>
2023-08-04 19:36:57 +0800

    Finals

---
+++ GMDEVAI Finals/Assets/Scripts/WASDMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public float rotationSpeed = 5;
+
+    void LateUpdate()
+    {
+        // Mouse Look
+        transform.Rotate(0, Input.GetAxis("Mouse X") * rotationSpeed, 0);
+
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(horizontal, 0, vertical);
+    }
+}
