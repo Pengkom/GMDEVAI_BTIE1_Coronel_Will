1c5963c81e3b0a9e32d01f41fd3de25b3e4bdbbd
Will Coronel <will.coronel@benilde.edu.ph>
2023-08-09 11:00:55 +0800

    [Finals] Added ObstacleSpawn (TODO: Timer)

---
+++ GMDEVAI Finals/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject obstacleSpawnPoint;
+    [SerializeField] private GameObject obstaclePrefab;
+
+    [SerializeField] private GameObject interactText;
+    [SerializeField] private GameObject stepText;
+
+    private bool playerInCollider;
+    private bool spawnObstacle;
+    private bool isSpawned;
+
+    private GameObject currentObstacle;
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = this.GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && playerInCollider) spawnObstacle = true;
+
+        if (spawnObstacle && !isSpawned)
+        {
+            interactText.SetActive(false);
+            stepText.SetActive(true);
+
+            if (!playerInCollider)
+            {
+                SpawnObstacle();
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!spawnObstacle) interactText.SetActive(true);
+        playerInCollider = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!spawnObstacle) interactText.SetActive(false);
+        playerInCollider = false;
+    }
+
+    private void SpawnObstacle()
+    {
+        currentObstacle = Instantiate(obstaclePrefab, obstacleSpawnPoint.transform.position, Quaternion.identity);
+        stepText.SetActive(false);
+        meshRenderer.enabled = false;
+        isSpawned = true;
+    }
+
+    private void ResetObstacle()
+    {
+        //TODO: Make Timer to despawn Obstacle ============================================================
+        
+        meshRenderer.enabled = true;
+        spawnObstacle = false;
+        isSpawned = false;
+    }
+}
